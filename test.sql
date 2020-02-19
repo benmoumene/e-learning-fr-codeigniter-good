bb384f79fba04b89e33e16686c56213f4a03418f
mickaelgudin <39730173+mickaelgudin@users.noreply.github.com>
2020-02-19 12:32:51 +0100

    Update fichier import base de données
    ajoute la création de la base de données en plus des tables

--- test.sql
+++ test.sql
@@ -22,6 +22,8 @@ SET time_zone = "+00:00";
 -- Base de données :  `test`
 --
 
+CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `test`;
 -- --------------------------------------------------------
 
 --
