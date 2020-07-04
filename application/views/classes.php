925a902905c7b25cb80c27a6ea57d41b44cc8d09
mickaelgudin <39730173+mickaelgudin@users.noreply.github.com>
2020-07-05 01:38:58 +0200

    ajout de l'url /api/classes & poc js, les classes sont maintenant affichés via une requete http

--- application/views/classes.php
+++ application/views/classes.php
@@ -1,41 +1,14 @@
-<?php $this->load->view("page_template/header"); ?>
+<?php $this->load->view("page_template/header"); 
+?>
 
 <div id="body" class="mbot">
     <card title="<?=($_SESSION['user'] === "admin") ? "Mes classes" : "" ?>">
     	
     	<div class="list-group mt-4">
     	                
-    	  <?php foreach($classeList as $classe) :?>
-          	
-          	<list-item lien="" titre="<?=$classe->getNom()?>" description=""></list-item>
-          	<?php if(sizeof($eleveList[$classe->getId()]) > 0) : ?>
-                <?php foreach($eleveList[$classe->getId()] as $eleve) : ?>
-                    <?php echo form_open("/ClassesController/removeEleve");?>
-                    <div class="table-responsive">
-          			<table class="table table-bordered table-dark">
-                     <thead>
-                        <tr>
-                          <td><?=$eleve->getPrenom()?></td>
-                          <td><?=$eleve->getNom()?></td>
-                          <td><?=$eleve->getEmail()?></td>
-                          <input type="text" name="eleve_id" value="<?=$eleve->getId()?>" hidden>
-                          <td><button class="btn btn-danger col-md-6 col-sm-2" onclick="return confirm('Etes vous sur de vouloir supprimer cet etudiant?')"><i class="fa fa-trash" style="font-size:30px;"></i></button>
- 						  </td>
-                        </tr>
-                      </thead>
-                    </table>
-                    </div>
-               	<?php echo form_close();?>	
-               	<?php endforeach;?>
-               	
-            <?php endif;?>
-            <?php echo form_open("/ClassesController/removeClasse");?>
-               	<button class="btn btn-danger mt-2 col-md-4 col-sm-2" onclick="return confirm('Etes vous sur de vouloir supprimer cette classe?')"><i class="fa fa-trash" style="font-size:30px;"></i> Supprimer la classe</button>
- 				<input type="text" name="classe_id" value="<?=$classe->getId()?>" hidden>
-                <hr>
-            <?php echo form_close();?>
-          <?php endforeach;?>
-        
+        <div class="classesList">
+        	
+        </div>
         <?php echo form_open("/ClassesController/createClasse");?>
 
 
@@ -54,6 +27,9 @@
 
 </div>
 <script src="/projetL3/application/views/page_template/components_vuejs/list_group.js"></script>
+
+
+
 <?php $this->load->view("page_template/footer");?>
 </body>
 </html>
--- application/views/page_template/components_vuejs/list_group.js
+++ application/views/page_template/components_vuejs/list_group.js
@@ -1,3 +1,41 @@
+var classesList = [];
+var elevesList = [];
+
+function getClasses () {
+  	makeGet('http://[::1]/projetL3/index.php/api/classes',classesList);
+}
+
+
+function makeGet(url, liste) {
+	const Http = new XMLHttpRequest();
+	Http.open("GET", url);
+	Http.send();
+
+	Http.onreadystatechange = (e) => {
+	  if (Http.readyState === 4) {
+		  liste = JSON.parse(Http.responseText);
+		  
+		  var divList = document.querySelector('.classesList');
+		  var ul = document.createElement('ul');
+		  
+		  liste.forEach(function(e){
+			  var li = document.createElement('li');
+			  li.innerText = e['nom'];
+			  li.classList = "list-group-item list-group-item-action flex-column align-items-start";
+			  
+			  ul.appendChild(li);
+			 
+		  });
+		  
+		  divList.appendChild(ul);
+		 
+		  console.log(liste);
+	  }
+	}
+}
+
+getClasses();
+
 let listItem = Vue.component('list-item', {
 	template: '<a v-bind:href="`${lien}`" class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">{{titre}}</h5></div><p class="mb-1">{{description}}</p></a>',
 	props: {
@@ -6,6 +44,9 @@ let listItem = Vue.component('list-item', {
 		'description' : String,
 	}
 });
+
+
+  
 
 new Vue({el : ".documents", component: listItem});
 new Vue({el : ".list-group", component: listItem});
